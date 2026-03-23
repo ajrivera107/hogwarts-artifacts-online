75d9019aba8d8e96c96d3667aa40a5dd85e40cd3
ajrivera107 <ashley.jh.rivera@gmail.com>
2026-03-23 16:15:12 -0500

    This is the first commit.

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonlinecda/HogwartsArtifactsOnlineCdaApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactsonlinecda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactsOnlineCdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsArtifactsOnlineCdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonlinecda/HogwartsArtifactsOnlineCdaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactsonlinecda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsOnlineCdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
