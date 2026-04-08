5686c3da5e244882e8a712462c80e66659598b9d
ajrivera107 <ashley.jh.rivera@gmail.com>
2026-04-08 01:36:56 -0500

    Connect to Azure Database for MySQL with Azure Key Vault. Database problem to be fixed.

--- src/test/java/edu/tcu/cs/hogwartsartifactsonlinecda/artifact/ArtifactServiceTest.java
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonlinecda/artifact/ArtifactServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles(value = "dev")
 class ArtifactServiceTest {
 
     @Mock
